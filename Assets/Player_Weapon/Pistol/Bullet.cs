a3a0989b33c4f85cf46f023dd1c9e6cf7b22e14e
ROriver1432 <jisoohyeon05@gmail.com>
2024-07-21 20:11:28 +0900

    권총 구현 수정본
    캐릭터 방향 관련 정보 필요, Platformer 태그가 붙은 물체를 맞출 때 총알이 사리지게함

--- Assets/Player_Weapon/Pistol/Bullet.cs
+++ Assets/Player_Weapon/Pistol/Bullet.cs
@@ -5,29 +5,17 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float BulletSpeed = 10.0f;
     Rigidbody2D Rigidbody;
-    public Vector3 spawnPoint;
     void Start()
     {
-        Rigidbody = GetComponent<Rigidbody2D>();
+        
     }
     private void Update()
     {
-        //캐릭터가 보는 방향 벡터를 정하고 RIgidbody로 힘을 가하는데 여기에 버그가 있음
-        if (Input.GetAxis("Horizontal")>0)
-        {
-            spawnPoint = Vector3.right;
-        }
-        if (Input.GetAxis("Horizontal")<0)
-        {
-            spawnPoint = Vector3.left;
-        }
-        Rigidbody.velocity = spawnPoint * BulletSpeed;
+        
     }
     
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         //총알이 Player 태그를 가지지 않는 물체를 맞추면 사라짐, Enemy 태그를 가진 물체를 맞추면 맞췄다는 문구가 뜨고 사라짐
         if (collision.gameObject.tag == "Enemy")
@@ -35,7 +23,9 @@ public class Bullet : MonoBehaviour
             Debug.Log("적을 맞춤");
             Destroy(this.gameObject);
         }
-        if(collision.gameObject.tag != "Player")
+
+        //벽이나 바닥에 닿았을 때(platform 태그를 가진 물체) 총알이 사라짐
+        if (collision.gameObject.tag == "Platform")
         {
             Destroy(this.gameObject);
         }
--- Assets/Player_Weapon/Pistol/Pistol.cs
+++ Assets/Player_Weapon/Pistol/Pistol.cs
@@ -7,8 +7,8 @@ public class Pistol : MonoBehaviour
     //공격력 변수 생성, 강화 능력 등으로 변경될 수 있는 수치
     public float attack = 1f;
     public float BulletSpeed = 8.0f;
-    public GameObject BulletPref;
-    public Transform BulletPos;
+    [SerializeField] GameObject BulletPref;
+    int rotation;
     void Start()
     {
         
@@ -16,9 +16,21 @@ public class Pistol : MonoBehaviour
 
     void Update()
     {
+        //나중에 방향에 대한 정보를 받을 시 수정 (임시 코드)
+        if(Input.GetAxis("Horizontal")>0)
+        {
+            rotation = 1;
+        }
+        else
+        {
+            rotation = -1;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject Bullet = Instantiate(BulletPref, BulletPos.position, transform.rotation);
+            GameObject Bullet = Instantiate(BulletPref, transform.position, transform.rotation);
+            Rigidbody2D rigidbody = Bullet.GetComponent<Rigidbody2D>();
+            rigidbody.velocity = Vector2.right * rotation * BulletSpeed;
         }
     }
 }
