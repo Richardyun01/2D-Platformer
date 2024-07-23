78de9b45bd826ff698a3562e52c4803bb5205b30
Richardyun01 <richardyun01@gmail.com>
2024-07-23 20:04:38 +0900

    added simple interactor

---
+++ Assets/2_World/Scripts/Mechanics/InteractionSystem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionSystem : MonoBehaviour
+{
+    public Transform detectionPoint;
+    private const float detectionRadius = 0.2f;
+    public LayerMask detectionLayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DetectObject())
+        {
+            if (InteractInput())
+            {
+                Debug.Log("INTERACT");
+            }
+        }
+    }
+
+    bool InteractInput()
+    {
+        return Input.GetKeyDown(KeyCode.E);
+    }
+
+    bool DetectObject()
+    {
+        return Physics2D.OverlapCircle(detectionPoint.position, detectionRadius, detectionLayer);
+    }
+}
