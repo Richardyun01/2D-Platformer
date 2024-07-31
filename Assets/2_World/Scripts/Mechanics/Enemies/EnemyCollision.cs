7563cc62943b93cc59a042dc58b23bf1e27a8bc7
Richardyun01 <richardyun01@gmail.com>
2024-07-31 12:41:19 +0900

    added level 1 and 2

---
+++ Assets/2_World/Scripts/Mechanics/Enemies/EnemyCollision.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    public int damage = 10;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+            }
+        }
+    }
+
+    public void setDamage(int damage)
+    {
+        this.damage = damage;
+    }
+}
+
